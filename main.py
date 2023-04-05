f577e454eb547b93f4adb6e00d86b2071e9f143d
155163GHUB <114282925+155163GHUB@users.noreply.github.com>
2023-04-05 20:37:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -55,7 +55,8 @@ def get_weather(region):
     # 天气
     weather = response["now"]["text"]
     # 当前温度
-    temp = response["now"]["temp"] + u"\N{DEGREE SIGN}" + "C"
+    temp = response["high"]["temp"] + u"\N{DEGREE SIGN}" + "C"
+    temp1 = response["little"]["temp"] + u"\N{DEGREE SIGN}" + "C"
     # 风向
     wind_dir = response["now"]["windDir"]
     return weather, temp, wind_dir
